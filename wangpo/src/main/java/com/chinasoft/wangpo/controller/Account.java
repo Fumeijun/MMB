35a86640ddcff76308a205717accc064bacf0c3d
makise_kurisuu@outlook.jp <mygvo0oq>
2017-06-14 23:05:19 +0800

    ver0.1.2

--- wangpo/src/main/java/com/chinasoft/wangpo/controller/Account.java
+++ wangpo/src/main/java/com/chinasoft/wangpo/controller/Account.java
@@ -1,9 +1,22 @@
 package com.chinasoft.wangpo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author Makise
  *         date 2017-06-14 17:31
  */
+@Controller
+@RequestMapping("/account")
 public class Account {
 
+    @RequestMapping("/login")
+    public String login(HttpServletRequest req, HttpServletResponse resp,Account account){
+
+        return null;
+    }
 }
---
+++ wangpo/src/main/java/com/chinasoft/wangpo/service/impl/AccountServiceImpl.java
@@ -0,0 +1,13 @@
+package com.chinasoft.wangpo.service.impl;
+
+import com.chinasoft.wangpo.controller.Account;
+import com.chinasoft.wangpo.service.AccountService;
+
+/**
+ * 2017-06-14 23:03
+ *
+ * @author Makise
+ */
+public class AccountServiceImpl extends BserServiceImpl<Account> implements AccountService {
+
+}
