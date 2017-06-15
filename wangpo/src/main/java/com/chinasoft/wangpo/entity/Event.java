0d34985a74324668a3130e31a2cede30a4c6e582
kurisu994 <makise_kurisuu@outlook.jp>
2017-06-15 14:49:45 +0800

    ver0.1.4

---
+++ wangpo/src/main/java/com/chinasoft/wangpo/entity/Event.java
@@ -0,0 +1,10 @@
+package com.chinasoft.wangpo.entity;
+
+/**
+ * @author Makise
+ *         date 2017-06-15 14:47
+ */
+public class Event {
+    private Integer e_id;
+    private Integer e_acc;
+}
