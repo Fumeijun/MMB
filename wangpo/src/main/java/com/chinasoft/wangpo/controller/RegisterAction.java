b987846585d7173bcccd6bb47f776836c2f1a02a
Administrator <Administrator@PC-20140222NQNK>
2017-06-16 12:00:47 +0800

    啦啦啦

--- wangpo/src/main/java/com/chinasoft/wangpo/controller/RegisterAction.java
+++ wangpo/src/main/java/com/chinasoft/wangpo/controller/RegisterAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.chinasoft.wangpo.entity.Register;
 import com.chinasoft.wangpo.service.RegisterService;
@@ -16,13 +17,15 @@ public class RegisterAction {
 	@Autowired//自动装配
 	private RegisterService registerService;
 	//检查账户名称
-	@RequestMapping("/checkName")
+	@RequestMapping(value="/checkName",produces="application/json;charset=utf-8")
+	@ResponseBody//返回json格式
 	public String checkName(HttpServletRequest req,HttpServletResponse resp,Register register) {
-		System.out.println("lalala"+register.getRname());
+		//System.out.println("lalala"+register.getRname());
 		String message="";
 		int count=registerService.checkName(register);
+		//System.out.println(count);
 		if(count>0){
-			message="该账户已存在";
+			message="{\"mes\":\"该账户已存在\"}";
 		}
 		return message;
 	}
